4c0b3a20a72a2b3b4f306831746ea111bfe4dde9
DanielCardenas199602 <gizmomaniac131@gmail.com>
2024-09-28 01:23:13 -0600

    Desarollo web
    proyecto de desarollo  web completo a nivel front-end

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $("#menu_ham").hide();
+
+    $("#ham").click(function(){
+
+        $("#menu_ham").show();
+      
+      });
+
+
+      $("#cierra").click(function(){
+  
+        $("#menu_ham").hide();
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
